9ebe15c21ffd74e2a41969e704ebbfbdfa66d734
eddyca <eddyca.rodriguez@gmail.com>
2015-03-14 18:08:07 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function(){
+  
+  var $container = $('#container'),
+      $body = $('body'),
+      colW = 280,
+      columns = null;
+  
+  $container.isotope({
+    // disable window resizing
+    resizable: false,
+    masonry: {
+      columnWidth: colW
+    }
+  });
+  
+
+  
+});
