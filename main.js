cb0c63cf08119fbce8b7e8d3a2222f760f28dd6d
eddyca <eddyca.rodriguez@gmail.com>
2015-03-17 23:14:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-$(function(){
-  
-  var $container = $('#container'),
-      $body = $('body'),
-      colW = 280,
-      columns = null;
-  
-  $container.isotope({
-    // disable window resizing
-    resizable: false,
-    masonry: {
-      columnWidth: colW
-    }
-  });
-  
+(function($){
 
-  
-});
+  var $doc = $(document),
+    $win = $(window);
+
+  $doc.on('ready', function(){
+        // document is ready
+        
+        $('#container').isotope();
+  });
+
+})(jQuery);
