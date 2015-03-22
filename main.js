3470969b4f0b1162c1febe2b0c4117b05df73ebd
eddyca <eddyca.rodriguez@gmail.com>
2015-03-22 10:21:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 
-// Simplified this a bit by just wrapping everything in a self-invoking anonymous function (so that your js won't conflict with any libraries)
 (function () {
 
-	$('#container').isotope({
-		itemSelector: '.portfolio',
-		masonry: {
-			// Added comma after '.portfolio'
-		  itemSelector: '.portfolio',
-      layoutMode: 'fitRows',
-		  gutter: 20
-		  // Removed extra parenthesis and semicolon after }
-		} 
-	});
+  $('#container').isotope({
+    itemSelector: '.portfolio',
+    masonry: {
+      itemSelector: '.portfolio',
+      resizable: false,
+      gutter: 20,
+      columnWidth: $('container').find('.portfolio')[0]
+    }
+  });
 
 }());
